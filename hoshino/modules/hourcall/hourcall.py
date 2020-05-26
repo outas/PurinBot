981401c40a52c8a2edb6e1b573c537387d19f3e0
outas <937023449@qq.com>
2020-05-27 01:03:51 +0800

    新增11点发送[睡觉.jpg]

--- hoshino/modules/hourcall/hourcall.py
+++ hoshino/modules/hourcall/hourcall.py
@@ -18,6 +18,11 @@ def get_hour_call():
 @sv.scheduled_job('cron', hour='*', )
 async def hour_call():
     now = datetime.now(pytz.timezone('Asia/Shanghai'))
+    if now.hour == 23:
+        await sv.broadcast(str(R.img('睡觉.jpg').cqcode), 'hourcall', 0)
+        return
+
     if not now.hour % 6 == 0:
         return
+        
     await sv.broadcast(str(R.img('买药.jpg').cqcode), 'hourcall', 0)
